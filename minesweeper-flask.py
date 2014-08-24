5b5d1df96f2a61fb67336e848645cacf1c1c91f6
buckie <wjmartino@gmail.com>
2014-08-24 14:54:05 -0400

    start of the app

--- minesweeper-flask.py
+++ minesweeper-flask.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.debug = True
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def start_page():
+    #return "Hello World!"
+    return render_template("new_game.html")
+
+@app.route('/new_game')
+def new_game():
+    return None
+
+@app.route('/board')
+def render_board():
+    return None
+
+@app.route('/select_space') #/<row>/<col>')
+def render_board(row, col):
+    return render_template("board.html", row, col)
+
+# @app.route('/board/<row>/<col>')
+# def select_space(row, col):
+#     # do something
+#     return None
 
 
 if __name__ == '__main__':
