4405937e3fddd509258695bf59b809d58c15b601
buckie <wjmartino@gmail.com>
2014-08-24 20:58:25 -0400

    hello world rendering
    Signed-off-by: buckie <wjmartino@gmail.com>

---
+++ forms.py
@@ -0,0 +1,9 @@
+__author__ = 'alex'
+
+from flask.ext.wtf import Form
+from wtforms import IntegerField, validators
+
+class NewGameForm(Form):
+    rows = IntegerField('rows', [validators.NumberRange(min=5, max=50), validators.DataRequired()])
+    columns = IntegerField('columns', [validators.NumberRange(min=5, max=50), validators.DataRequired()])
+    mines = IntegerField('mines', [validators.NumberRange(min=5, max=99), validators.DataRequired()])
--- minesweeper-flask.py
+++ minesweeper-flask.py
@@ -1,15 +1,23 @@
-from flask import Flask, render_template, request, redirect, url_for
-from minesweeper import NewGameForm
+from flask import Flask, render_template, request, redirect, url_for, g
+from forms import NewGameForm
+import minesweeper as ms
 
 app = Flask(__name__)
 app.debug = True
 app.config.from_object('config')
 
+# global game board -- one player only
+global game_board
+game_board = None
+global game_on
+game_on = False
+
 
 @app.route('/')
 def start_page():
-    #return "Hello World!"
+    # return "Hello World!"
     return redirect('/new_game')
+
 
 @app.route('/new_game', methods=['GET', 'POST'])
 def new_game():
@@ -18,22 +26,37 @@ def new_game():
         rows = form.rows.data
         columns = form.columns.data
         mines = form.mines.data
-        return redirect(url_for('render_board'))
+        if mines < rows * columns:
+            global game_board
+            game_board = ms.create_game_board(rows, columns, mines=mines)
+            global game_on
+            game_on = True
+            return redirect(url_for('render_board'))
+
     return render_template('new_game.html', form=form)
 
 
 @app.route('/board')
 def render_board():
-    return None
+    if not game_on:
+        redirect(url_for('new_game'))
+    return render_template("board.html", game_board=game_board)
 
-# @app.route('/select_space') #/<row>/<col>')
-# def render_board(row, col):
-#     return render_template("board.html", row, col)
+@app.route('/select_space/<int:row>/<int:col>')
+def select_space(row, col):
+    global game_board
+    global game_on
 
-# @app.route('/board/<row>/<col>')
-# def select_space(row, col):
-#     # do something
-#     return None
+    if game_on:
+        if 0 <= row <= len(game_board) and 0 <= col <= len(game_board[0]):
+            #Valid entry
+            game_on, game_board = ms.update_board(game_board, row, col)
+        if game_on:
+            return redirect(url_for('render_board'))
+        else:
+            return redirect(url_for('new_game'))
+    else:
+        return redirect(url_for('new_game'))
 
 
 if __name__ == '__main__':
